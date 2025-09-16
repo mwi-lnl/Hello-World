58a34bd38e2f53700f348b18f99acad373fce778
maurizio_wille <maurizio.wille@loterie.lu>
2025-09-16 16:27:53 +0200

    Second commit

---
+++ ConsoleTestRegistrySol/ConsoleTestRegistry/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(string.Format("Today we are {0:dd/MM/yyyy}", DateTime.Now));
